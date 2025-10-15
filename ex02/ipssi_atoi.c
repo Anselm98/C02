cebbf2144c63cc0e5f2ac6bb8af56aa83f476e36
Clement Maroy <clement.maroy@gmail.com>
2025-10-15 17:15:51 +0200

    ex02

---
+++ ex02/ipssi_atoi.c
@@ -0,0 +1,28 @@
+int	ipssi_atoi(char *str)
+{
+	int	result;
+	int	sign;
+	int	i;
+
+	result = 0;
+	sign = 1;
+	i = 0;
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
+	{
+		i++;
+	}
+	while (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+		{
+			sign = sign * -1;
+		}
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = (result * 10) + (str[i] - '0');
+		i++;
+	}
+	return (result * sign);
+}
